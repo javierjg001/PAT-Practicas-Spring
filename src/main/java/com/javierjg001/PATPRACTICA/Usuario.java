07a6ac4a696af9c363fdcb39ba418e21245ae1c1
javierjg001 <56931870+javierjg001@users.noreply.github.com>
2022-03-20 20:38:10 +0100

    Subida registro

---
+++ src/main/java/com/javierjg001/PATPRACTICA/Usuario.java
@@ -0,0 +1,44 @@
+package com.javierjg001.PATPRACTICA;
+
+public class Usuario {
+    
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String contrasena;
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido1() {
+        return apellido1;
+    }
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+    public String getApellido2() {
+        return apellido2;
+    }
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+    public String getcontrasena() {
+        return contrasena;
+    }
+    public void setcontrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+     
+    
+}
