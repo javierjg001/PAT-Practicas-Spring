8f69aba69c993adcd3e5298ce31725fbb454c55a
javierjg001 <56931870+javierjg001@users.noreply.github.com>
2022-03-20 18:23:22 +0100

    Primera subida

---
+++ src/test/java/com/javierjg001/PDSPRACTICA/PdsPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierjg001.PDSPRACTICA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
