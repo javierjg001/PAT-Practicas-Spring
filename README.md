eb88daa01c4881fb2729eed02db2af4c883b7d55
javierjg001 <56931870+javierjg001@users.noreply.github.com>
2022-03-20 19:02:58 +0100

    Cambio de PDS a PAT

--- README.md
+++
@@ -1,4 +0,0 @@
-# PDS-PRACTICA5
-PAT PRACTICA 5
-
-[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/javierjg001/PDS-PRACTICA5/edit/main/README.md)
---
+++ src/test/java/com/javierjg001/PATPRACTICA/PatPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierjg001.PATPRACTICA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
